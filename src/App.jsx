d06937c780415e55638c4661f7b11f8680fa13ee
Kenneth Stallworth <kennethstallworth@hotmail.com>
2024-05-13 10:29:00 -0400

    updated commit of redux practice app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BabyMath from './babyMath'
+import NumberInputs from "./NumberInputs"
 function App() {
 
   return (
     <>return
       <BabyMath />
+      <NumberInputs />
     </>
   )
 }
--- src/NumberInputs.jsx
+++ src/NumberInputs.jsx
@@ -1,30 +1,49 @@
 import React from 'react'
+import {useState} from "react"
+import {useDispatch, useSelector} from 'react-redux'
 
 const NumberInputs = () =>{
+	const dispatch = useDispatch()
+	const [inputs,setInputs]=useState({num1:0,num2:0}
+
+	const handleChange = e =>{
+		const {value , name} = e.target
+		setInputs(inputs =>({...inputs,[name]:value}))
+	}
+
+
+
+	const changeNum = (num)=>{
+		dispatch({
+			type:"CHANGE_NUM",
+			num,
+			value: inputs[num]
+		})
+	}
    return(
-		
+	<>	
      <div>
-
-        <label htmlFor="num1>First FOOKING NUMBER:</label>
+        
+        <label htmlFor="num1">First FOOKING NUMBER:</label>
+	  <input type="number" id="num1"  value={inputs.num1}
+	  onChange={handleChange},
+	  name="num1"
+	  />
+	  <button onClick={()=>changeNum('num1')}>Update</button>
+	</div>
+     <div>
+	        <label htmlFor="num2">Seond FOOKING NUMBER:</label>
 	  <input 
 		type="number" 
-		id="num1" 
-		value=
-		onChange=
+		id="num2",
+		 value={inputs.num2},
+		 onChange={handleChange}
+		 name="num2"
 		/>
-	  <button onClick={}>Update</button>
+	  <button onClick={()=>changeNum('num2')}>Update</button>
 
-
-	        <label htmlFor="num1>Second FOOKING NUMBER:</label>
-	  <input 
-		type="number" 
-		id="num1" 
-		value=
-		onChange=
-		/>
-	  <button onClick={}>Update</button>
-
-	</div>   
+	</div>  
+</> 
 	)
 }
 
